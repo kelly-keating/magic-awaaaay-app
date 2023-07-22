fd333d5771bff10b8bbeadf44f91dac936681f73
Kelly Keating <keating.kel@gmail.com>
2023-07-22 22:36:13 +1200

    init

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, FormLabel, FormInput, Button } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+          <View>
+                <FormLabel>Email</FormLabel>
+                <FormInput />
+                <FormLabel>Password</FormLabel>
+                <FormInput/>
+              <Button>
+                <Text>Login</Text>
+              </Button>
+          </View>
+        
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
